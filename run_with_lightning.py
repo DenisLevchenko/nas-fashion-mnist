68bba6a8d20cdcfb027ab89fbe12d28f02337f5b
Denis Levchenko <levchenkodenis@gmail.com>
2026-06-01 15:20:23 +0200

    much neater unified LitModule, config classes

--- run_with_lightning.py
+++ run_with_lightning.py
@@ -16,16 +16,19 @@ from torchvision.transforms import ToTensor, Lambda
 from torchmetrics.classification import MulticlassAccuracy
 import lightning as L
 from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
+from lightning.pytorch.cli import LightningCLI
 import matplotlib.pyplot as plt
 from typing import Tuple, List, Union
 from architectures import MLP, CNN
+from dataclasses import dataclass
+
 
 # define the LightningModule
-class MLPLight(L.LightningModule):
-    def __init__(self, n_hidden: int, size_hidden: int, learning_rate: float):
+class LitModule(L.LightningModule):
+    def __init__(self, model: nn.Module, learning_rate: float):
         super().__init__()
         self.save_hyperparameters()
-        self.net = MLP(n_hidden=n_hidden, size_hidden=size_hidden)
+        self.net = model
         self.learning_rate = learning_rate
         self.loss = nn.CrossEntropyLoss()
         self.accuracy = MulticlassAccuracy(num_classes=10)
@@ -64,58 +67,48 @@ class MLPLight(L.LightningModule):
         return optimizer
 
 
-class CNNLight(L.LightningModule):
-    def __init__(self, out_channels: int, kernel_size: int, padding: str,
-                 dilation: int, act_function, dropout_rate: float,
-                 learning_rate: float):
-        super().__init__()
-        self.save_hyperparameters()
-        self.net = CNN(out_channels=out_channels, kernel_size=kernel_size,
-                       padding=padding, dilation=dilation, act_function=act_function,
-                       dropout_rate=dropout_rate)
-        self.learning_rate = learning_rate
-        self.loss = nn.CrossEntropyLoss()
-        self.accuracy = MulticlassAccuracy(num_classes=10)
-        
-    def training_step(self, batch, batch_idx):
-        # training_step defines the train loop.
-        # it is independent of forward
-        x, y = batch
-        z = self.net(x)
-        loss = self.loss(z, y)
-        # Logging to TensorBoard (if installed) by default
-        self.log("train_loss", loss)
-        return loss
-    
-    def validation_step(self, batch, batch_idx):
-        # this is the validation loop
-        x, y = batch
-        z = self.net(x)
-        val_loss = self.loss(z, y)
-        val_acc = self.accuracy(z, y.argmax(dim=1))
-        self.log("val_loss", val_loss, prog_bar=True)
-        self.log("val_accuracy", val_acc, prog_bar=True)
-        
-    def test_step(self, batch, batch_idx):
-        # this is the test loop
-        x, y = batch
-        z = self.net(x)
-        test_loss = self.loss(z, y)
-        test_acc = self.accuracy(z, y.argmax(dim=1))
-        self.log("test_loss", test_loss)
-        self.log("test_accuracy", test_acc)
-        # self.test_step_outputs.append(test_acc)
+@dataclass
+class MLPConfig:
+    n_hidden: int = 3
+    size_hidden: int = 64
 
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        return optimizer
+
+@dataclass
+class CNNConfig:
+    out_channels: int = 8
+    kernel_size: int = 3
+    padding: str = 'same'
+    dilation: int = 0
+    act_function = nn.ReLU()
+    dropout_rate: float = 0.2
+
+
+@dataclass
+class ModelConfig:
+    name: str = "mlp"
+    cfg: object = MLPConfig()
+
+
+@dataclass
+class TrainConfig:
+    lr: float = 1e-3
+    batch_size: int = 32
+
+
+def build_model(model_config):
+    if model_config.name == "mlp":
+        return MLP(**model_config.cfg.__dict__)
+    elif model_config.name == "cnn":
+        return CNN(**model_config.cfg.__dict__)
+    else:
+        raise ValueError(f"Unknown model: {model_config.name}")
 
 
 def main():
-        # init the model
-    # model = MLPLight(n_hidden=3, size_hidden=64, learning_rate=1e-3)
-    model = CNNLight(out_channels=8, kernel_size=3, padding='same', dilation=0,
-                      act_function=nn.ReLU(), dropout_rate=0.2, learning_rate=1e-3)
+    # init the PyTorch model
+    model = build_model(ModelConfig())
+    # init the LightningModule
+    lit_module = LitModule(model=model, learning_rate=TrainConfig().lr)
     
     # setup data
     training_data = datasets.FashionMNIST(
@@ -169,12 +162,11 @@ def main():
         verbose=False
     )
     trainer = L.Trainer(callbacks=[checkpoint_callback, early_stop_callback], max_epochs=50)
-    trainer.fit(model, train_loader, val_loader)
+    trainer.fit(lit_module, train_loader, val_loader)
     
     # Load and test the best model from checkpoint
     best_model_path = checkpoint_callback.best_model_path
-    # best_model = MLPLight.load_from_checkpoint(best_model_path)
-    best_model = CNNLight.load_from_checkpoint(best_model_path)
+    best_model = LitModule.load_from_checkpoint(best_model_path)
     trainer.test(best_model, dataloaders=test_loader)
 
 
